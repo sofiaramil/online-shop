71209871dc6ea8f6a87ad15096824a8dfecd5aa1
sofiaramil <sofiaramilkyzy@example.com>
2022-12-13 21:55:10 +0600

    fix bag

--- online.shop/src/App.js
+++ online.shop/src/App.js
@@ -1,65 +1,57 @@
-import React, { useEffect } from 'react';
+import React from 'react'
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import Header from './Header';
-import Home from './Home';
-import Login from './Login';
-import { useStateValue } from './store/stateProvider';
-import { auth } from './config/firebase';
-import Checkout from './Checkout';
-import Pay from './Pay';
-import Orders from './Orders';
-
-const promise = loadStripe(process.env.REACT_APP_PUBLIC_KEY_STRIPE);
+import {Router,Route,Switch} from 'react-router-dom'
+import Layout from './Pages/Layout/layout'
+import Home from './Pages/Home'
+import Men from './Pages/Men'
+import Women from './Pages/Women'
+import Slippers from './Pages/Slippers'
+import Addidas from './Mens-collection/Addidas'
+import AirJordan from './Mens-collection/AirJordan'
+import Balenciaga from './Mens-collection/Balenciaga'
+import Casual from './Mens-collection/Casual'
+import Gucci from './Mens-collection/Gucci'
+import NewBalance from './Mens-collection/NewBalance'
+import Nike from './Mens-collection/Nike'
+import Sky from './Mens-collection/Sky'
+import SuperMax from './Mens-collection/SuperMax'
+import Vans from './Mens-collection/Vans'
+import Login from './component/Login'
+import Register from './component/Register'
+import _404 from './Pages/_404'
+import Details from './Pages/details'
+import history from './Pages/history'
+import ShoeContextProvider from './context/shoeContext'
 
 function App() {
-  const [, dispatch] = useStateValue();
-
-  useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        dispatch({
-          type: 'SET_USER',
-          user: authUser
-        });
-      } else {
-        dispatch({
-          type: 'SET_USER',
-          user: null
-        });
-      }
-    });
-  }, []);
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/checkout">
-            <Header />
-            <Checkout />
-          </Route>
-          <Route path="/orders">
-            <Header />
-            <Orders />
-          </Route>
-          <Route path="/payment">
-            <Header />
-            <Elements stripe={promise}>
-              <Pay />
-            </Elements>
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/">
-            <Header />
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+    <ShoeContextProvider>
+    <Router history={history}>
+    <Layout>
+    <Switch>
+       <Route exact path="/" component={Home}></Route>
+       <Route path="/Men" component={Men}></Route>
+       <Route path="/Women" component={Women}></Route>
+       <Route path="/Slippers" component={Slippers}></Route>
+       <Route path="/shoe:id" component={Details}></Route>
+       <Route path="/Addidas" component={Addidas}></Route>
+       <Route path="/Air Jordan" component={AirJordan}></Route>
+       <Route path="/Balenciaga" component={Balenciaga}></Route>
+       <Route path="/Casual" component={Casual}></Route>
+       <Route path="/Gucci" component={Gucci}></Route>
+       <Route path="/New Balance" component={NewBalance}></Route>
+       <Route path="/Nike" component={Nike}></Route>
+       <Route path="/Sky" component={Sky}></Route>
+       <Route path="/Super Max" component={SuperMax}></Route>
+       <Route path="/Vans" component={Vans}></Route>
+       <Route path="/login" component={Login}></Route>
+       <Route path="/Register" component={Register}></Route>
+       <Route path="**" component={_404}></Route>
+    </Switch>
+    </Layout>
     </Router>
+    </ShoeContextProvider>
+
   );
 }
 
